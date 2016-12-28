94b4e7824a4452329069f5cd17c129f2179bbcd1
Kiks Kanapi <fokanapi@gmail.com>
2016-12-28 16:26:27 +0800

    implementing async

--- routes/pets.js
+++ routes/pets.js
@@ -2,16 +2,54 @@ var r = require("request").defaults({
     json: true
 });
 
-module.exports = function(app){
+var async = require("async");
+
+module.exports = function (app) {
     app.get(
-        '/pets', function(req, res){
-            r({uri:"http://localhost:3001/dog"}, function(error, response, body){
-                if(!error && response.statusCode === 200){
-                    res.json(body);
-                }else{
-                    res.send(response.statusCode);
+        '/pets',
+        function (req, res) {
+            async.parallel({
+                    dogs: function (callback) {
+                        r({
+                                uri: "http://localhost:3001/dog"
+                            },
+                            function (error, response, body) {
+                                if (error) {
+                                    callback({
+                                        service: 'dog',
+                                        error: error
+                                    });
+                                } else if (!error && response.statusCode === 200) {
+                                    callback(null, body);
+                                } else {
+                                    callback(response.statusCode);
+                                }
+                            });
+                    },
+                    cats: function (callback) {
+                        r({
+                                uri: "http://localhost:3000/cat"
+                            },
+                            function (error, response, body) {
+                                if (error) {
+                                    callback({
+                                        service: 'cat',
+                                        error: error
+                                    });
+                                } else if (!error && response.statusCode === 200) {
+                                    callback(null, body);
+                                } else {
+                                    callback(response.statusCode);
+                                }
+                            });
+                    }
+                },
+                function (error, result) {
+                    res.send({
+                        error: error,
+                        result: result
+                    });
                 }
-            });
-        }
-    );
+            );
+        });
 };
